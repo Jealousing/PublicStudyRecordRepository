8d24d3f007e56d19e07f0a8300b30706cf87cc65
Jealousing <62935159+Jealousing@users.noreply.github.com>
2023-05-20 14:07:18 +0900

    심화 1레벨 시작

---
+++ Baekjoon/Advanced Level 1/25083.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+/*
+Problem Number: 25083
+
+Problem Description :
+아래 예제와 같이 새싹을 출력하시오.
+
+Link: https://www.acmicpc.net/problem/25083
+
+Input:
+none 
+
+Output:
+새싹을 출력한다.
+
+Limit: none
+*/
+
+int main()
+{
+    cout << "         ,r'\"7" << endl;
+    cout << "r`-_   ,'  ,/" << endl;
+    cout << " \\. \". L_r'" << endl;
+    cout << "   `~\\/" << endl;
+    cout << "      |" << endl;
+    cout << "      |" << endl;
+   
+
+    return 0;
+}
