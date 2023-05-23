e9fb984f8ac4fbe102ab6bdfe93d0def043f718f
Jealousing <62935159+Jealousing@users.noreply.github.com>
2023-05-23 18:11:24 +0900

    정렬 9/11

---
+++ Baekjoon/Sorting/10989.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+/*
+Problem Number: 10989
+
+Problem Description :
+N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+
+Link: https://www.acmicpc.net/problem/10989
+
+Input:
+첫째 줄에 수의 개수 N(1 ≤ N ≤ 10,000,000)이 주어진다. 둘째 줄부터 N개의 줄에는 수가 주어진다. 이 수는 10,000보다 작거나 같은 자연수이다.
+
+Output:
+첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
+
+Limit: none
+*/
+
+
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0);
+	int N;
+	int temp = 0;
+	int value[10001]{0,};
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> temp;
+		value[temp] ++;
+	}
+
+	for (int i = 0; i < 10001; i++)
+	{
+		for (int j = 0; j < value[i]; j++)
+		{
+			cout << i << '\n';
+		}
+	}
+
+	return 0;
+}
---
+++ Baekjoon/Sorting/1427.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+/*
+Problem Number: 1427
+
+Problem Description :
+배열을 정렬하는 것은 쉽다. 수가 주어지면, 그 수의 각 자리수를 내림차순으로 정렬해보자.
+
+Link: https://www.acmicpc.net/problem/1427
+
+Input:
+첫째 줄에 정렬하려고 하는 수 N이 주어진다. N은 1,000,000,000보다 작거나 같은 자연수이다.
+
+Output:
+첫째 줄에 자리수를 내림차순으로 정렬한 수를 출력한다.
+
+Limit: none
+*/
+
+
+
+int main()
+{
+	
+	string N;
+	cin >> N;
+
+	for (int i = 0; i < N.size() - 1; i++)
+	{
+		for (int j = i + 1; j < N.size(); j++)
+		{
+			if (N[i] < N[j])
+			{
+				int temp = N[i];
+				N[i] = N[j];
+				N[j] = temp;
+			}
+		}
+	}
+
+	cout << N << endl;
+
+	return 0;
+}
