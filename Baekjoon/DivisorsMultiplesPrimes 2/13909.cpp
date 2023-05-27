656e965606457b555dec8932091471200fa1b0c8
Jealousing <62935159+Jealousing@users.noreply.github.com>
2023-05-27 20:47:53 +0900

    약수, 배수와 소수 2 끝

---
+++ Baekjoon/DivisorsMultiplesPrimes 2/13909.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+using namespace std;
+
+/*
+Problem Number: 13909
+
+Problem Description :
+서강대학교 컴퓨터공학과 실습실 R912호에는 현재 N개의 창문이 있고 또 N명의 사람이 있다. 1번째 사람은 1의 배수 번째 창문을 열려 있으면 닫고 닫혀 있으면 연다.  
+2번째 사람은 2의 배수 번째 창문을 열려 있으면 닫고 닫혀 있으면 연다. 이러한 행동을 N번째 사람까지 진행한 후 열려 있는 창문의 개수를 구하라. 단, 처음에 모든 창문은 닫혀 있다.
+
+예를 들어 현재 3개의 창문이 있고 3명의 사람이 있을 때,
+1번째 사람은 1의 배수인 1,2,3번 창문을 연다. (1, 1, 1)
+2번째 사람은 2의 배수인 2번 창문을 닫는다. (1, 0, 1)
+3번째 사람은 3의 배수인 3번 창문을 닫는다. (1, 0, 0)
+결과적으로 마지막에 열려 있는 창문의 개수는 1개 이다.
+
+Link: https://www.acmicpc.net/problem/13909
+
+Input:
+첫 번째 줄에는 창문의 개수와 사람의 수 N(1 ≤ N ≤ 2,100,000,000)이 주어진다.
+
+Output:
+각각의 테스트 케이스마다 골드바흐 파티션의 수를 출력한다.
+
+Limit: none
+*/
+
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	unsigned int N,count=0;
+	cin >> N;
+
+	// 1~N까지 제곱수의 개수 = 열려있는 창문
+	for (int i = 1; i * i <= N; ++i)
+		count++;
+	cout << count;
+
+	/*
+	메모리 초과 
+	-> 패턴확인
+	#define MAX  2100000001
+	vector<bool> window(MAX, false);
+	for (int i = 1; i < N; i++)
+	{
+		for (int j = i; j < N; j += i)
+		{
+			if (window[j])
+			{
+				window[j] = false;
+				count--;
+			}
+			else
+			{
+				window[j] = true;
+				count++;
+			}
+		}
+	}
+
+	cout << count << '\n';
+
+	for (int i = 0; i < N; i++)
+	{
+		if (window[i]) cout << i << ' ';
+	}
+	*/
+	
+	return 0;
+}
---
+++ Baekjoon/DivisorsMultiplesPrimes 2/17103.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+using namespace std;
+
+/*
+Problem Number: 17103
+
+Problem Description :
+골드바흐의 추측: 2보다 큰 짝수는 두 소수의 합으로 나타낼 수 있다.
+짝수 N을 두 소수의 합으로 나타내는 표현을 골드바흐 파티션이라고 한다. 짝수 N이 주어졌을 때, 골드바흐 파티션의 개수를 구해보자. 두 소수의 순서만 다른 것은 같은 파티션이다.
+
+Link: https://www.acmicpc.net/problem/17103
+
+Input:
+첫째 줄에 테스트 케이스의 개수 T (1 ≤ T ≤ 100)가 주어진다. 각 테스트 케이스는 한 줄로 이루어져 있고, 정수 N은 짝수이고, 2 < N ≤ 1,000,000을 만족한다.
+
+Output:
+각각의 테스트 케이스마다 골드바흐 파티션의 수를 출력한다.
+
+Limit: none
+*/
+
+#define MAX 1000001
+bool prime[MAX] = { false, };
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	//에라토스테네스의 체
+	for (int i = 0; i < MAX; i++) 
+	{
+		prime[i] = true;
+	}
+	for (int j = 2; j < MAX; j++)
+	{
+		if (!prime[j]) continue;
+		for (int k = j + j; k < MAX; k += j)
+		{
+			prime[k] = false;;
+		}
+	}
+
+	int T,target, temp,count=0;
+	cin >> T;
+	for (int i = 0; i < T; i++)
+	{
+		cin >> target;
+
+		temp = target / 2;
+		for (int j = 2; j <= temp; j++)
+		{
+			if (!prime[j]) continue;
+
+			if (prime[target - j])
+			{
+				count++;
+			}
+		}
+
+		cout << count << '\n';
+		count = 0;
+	}
+
+
+	return 0;
+}
