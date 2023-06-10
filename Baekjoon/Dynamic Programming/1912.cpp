7797f93763893b2f9275e7de3d2c1abcb165670b
Jealousing <62935159+Jealousing@users.noreply.github.com>
2023-06-10 20:37:42 +0900

    2문제 추가

---
+++ Baekjoon/Dynamic Programming/1912.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+/*
+Problem Number: 1912
+
+Problem Description :
+n개의 정수로 이루어진 임의의 수열이 주어진다. 우리는 이 중 연속된 몇 개의 수를 선택해서 구할 수 있는 합 중 가장 큰 합을 구하려고 한다.
+단, 수는 한 개 이상 선택해야 한다.
+예를 들어서 10, -4, 3, 1, 5, 6, -35, 12, 21, -1 이라는 수열이 주어졌다고 하자. 여기서 정답은 12+21인 33이 정답이 된다.
+
+Link: https://www.acmicpc.net/problem/1912
+
+Input:
+첫째 줄에 정수 n(1 ≤ n ≤ 100,000)이 주어지고 둘째 줄에는 n개의 정수로 이루어진 수열이 주어진다. 
+수는 -1,000보다 크거나 같고, 1,000보다 작거나 같은 정수이다.
+
+Output:
+첫째 줄에 답을 출력한다.
+
+Limit: none
+*/
+
+
+#define MAXSIZE 100001
+long long int arr[MAXSIZE];
+int maxValue = -1001,n;
+
+void maxCheck()
+{
+    int sum = arr[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        // 진행해야될 합보다 새로운 값이 더 클경우 재설정
+        if (sum + arr[i] < arr[i])
+        {
+            sum = arr[i];
+        }
+        else
+        {
+            sum += arr[i];
+        }
+
+        if (maxValue < sum) maxValue = sum;
+    }
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        if (maxValue < arr[i])
+        {
+            maxValue = arr[i];
+        }
+    }
+
+    maxCheck();
+    cout << maxValue << '\n';
+
+    return 0;
+}
---
+++ Baekjoon/Dynamic Programming/9461.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+/*
+Problem Number: 9461
+
+Problem Description :
+오른쪽 그림과 같이 삼각형이 나선 모양으로 놓여져 있다. 첫 삼각형은 정삼각형으로 변의 길이는 1이다. 그 다음에는 다음과 같은 과정으로 정삼각형을 계속 추가한다. 
+나선에서 가장 긴 변의 길이를 k라 했을 때, 그 변에 길이가 k인 정삼각형을 추가한다.
+파도반 수열 P(N)은 나선에 있는 정삼각형의 변의 길이이다. P(1)부터 P(10)까지 첫 10개 숫자는 1, 1, 1, 2, 2, 3, 4, 5, 7, 9이다.
+N이 주어졌을 때, P(N)을 구하는 프로그램을 작성하시오.
+https://www.acmicpc.net/upload/images/pandovan.png
+
+Link: https://www.acmicpc.net/problem/9461
+
+Input:
+첫째 줄에 테스트 케이스의 개수 T가 주어진다. 각 테스트 케이스는 한 줄로 이루어져 있고, N이 주어진다. (1 ≤ N ≤ 100)
+
+Output:
+각 테스트 케이스마다 P(N)을 출력한다.
+
+Limit: none
+*/
+
+/*
+1, 1, 1, 2, 2, 3, 4, 5, 7, 9
+
+arr[i] = arr[i-2]+arr[i-3];
+*/
+
+#define MAXSIZE 1001
+long long int arr[MAXSIZE];
+int checkPoint = 4;
+
+long long int P(int n)
+{
+    if (arr[n] != 0) return arr[n];
+    
+    for (int i = checkPoint; i <= n; i++)
+    {
+        arr[i] = (arr[i - 2] + arr[i - 3]);
+    }
+    checkPoint = n;
+    return arr[n];
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    arr[1] = 1, arr[2] = 1, arr[3]=1;
+    int n,testCase;
+
+    cin >> testCase;
+
+    for (int i = 0; i < testCase; i++)
+    {
+        cin >> n;
+        cout << P(n) << '\n';
+    }
+
+    return 0;
+}
